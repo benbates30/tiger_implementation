ac96034db8d8faf7984f2d69a555b39cc2d94d39
Benjamin Bates <benbates30@gmail.com>
2023-06-06 00:35:21 -0400

    new training script for rqvae

--- train_rqvae.py
+++ train_rqvae.py
@@ -1,15 +1,10 @@
-from sentence_transformers import SentenceTransformer
 from src.rqvae.rqvae import RQVAE
 import json
 import torch
 from torch import nn
-from torch.optim import adagrad
-from torch.utils.data import TensorDataset, DataLoader
-
-"""
-Create item embeddings using SentenceT5, then train rqvae on the dataset of item embeddings.
-"""
-
+from torch.optim import Adagrad
+from torch.utils.data import DataLoader
+from embedding_dataset import EmbeddingDataset
 
 """
 Trained for 20k epochs, achieves >= 80% codebook usage. Adagrad Optimizer with lr=0.4.
@@ -19,36 +14,18 @@ Batch size = 1024
 ########   HYPERPARAMETERS  ##############
 EPOCHS=20000
 BATCH_SIZE = 1024
+PRINT_LOSS_INTERVAL = 100
+model_path = "models/rqvae.pt"
+item_feat_dir = "data/beauty"
+item_context_file = "item_feat.json"
+item_embed = "item_embeddings.pt"
 
 
 def load_json(file_path):
     with open(file_path, "r") as f:
         return json.load(f)
-    
-file_path = "data/beauty/item_feat.json"
-item_sentences = load_json(file_path)
 
-
-model = SentenceTransformer('sentence-transformers/sentence-t5-base')
-
-sentence_list = list(item_sentences.values())
-
-# Modify the list of sentences using the modify_sentences function
-modified_sentence_list = model.encode(sentence_list)
-
-# Map the modified sentences back to their original ids in the dictionary
-item_embeddings = {}
-for id, sentence in item_sentences.items():
-    item_embeddings[id] = modified_sentence_list[sentence_list.index(sentence)]
-
-
-
-
-
-
-########
-
-dataset = TensorDataset(torch.tensor(item_embeddings.values()))
+dataset = EmbeddingDataset(item_feat_dir, item_context_file, item_embed)
 train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 # initalizing RQVAE
@@ -60,14 +37,15 @@ vae = RQVAE(
     hidden_channels=[512, 256, 128]
 )
 
-optimizer = adagrad(vae.parameters(), lr=0.4)
+optimizer = Adagrad(vae.parameters(), lr=0.4)
 
+print("Training...")
 for epoch in range(1, EPOCHS+1):
     epoch_loss = 0
     epoch_recon_loss = 0
     epoch_rqvae_loss = 0
     for data in train_loader:
-        data = data[0]
+        data = data
         optimizer.zero_grad()
         out, codes, quant_loss = vae(data)
         loss, recon_loss, rqvae_loss = vae.compute_loss(out, data, quant_loss)
@@ -80,10 +58,12 @@ for epoch in range(1, EPOCHS+1):
     
     
 
-    if epoch % 50 == 0:
+    if epoch % PRINT_LOSS_INTERVAL == 0:
         print(f"Epoch {epoch}: Loss: {epoch_loss} Recon: {epoch_recon_loss}, Rqvae: {epoch_rqvae_loss}")
 
-    
+
+
+torch.save(vae.state_dict(), model_path)
     
 
 
